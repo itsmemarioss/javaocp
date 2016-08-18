38d1c51367afb04de5ace38ea7114ad81f752584
Mário Santos <super.mario.santos@gmail.com>
2016-08-17 21:22:08 -0300

    testing IO

---
+++ src/io/Animal.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable{
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Animal(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [name=" + name + "]";
+	}
+
+	
+	
+}
---
+++ src/io/CopyFileSampleWithBuffer.java
@@ -0,0 +1,37 @@
+package io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * based on the book OCP
+ * Using Buffered{Input|Output}Stream
+ * @author mario
+ *
+ */
+public class CopyFileSampleWithBuffer {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+//		if file already exists, it will be overridden by this code
+		File source = new File("test");
+		File destination = new File("test_copy");
+		
+//		the resources will be closed at the end by the try-with-resources
+		try(InputStream is = new BufferedInputStream( new FileInputStream(source));
+				OutputStream os = new BufferedOutputStream(new FileOutputStream(destination))){
+			byte[] buffer = new byte[1024];
+			int lengthRead;
+			while ( (lengthRead = is.read(buffer))> 0 ) {
+				os.write(buffer, 0, lengthRead);
+				os.flush();
+			}
+		}
+	}
+}
